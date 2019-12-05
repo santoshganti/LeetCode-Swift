1ed79d60c0091b9f6ac03d9bb1ce20d9bd07c4a9
Santosh Ganti <santosh.ganti@me.com>
2019-12-04 21:05:07 -0500

    Initial commit

---
+++ LeetCode-Swift/1.LengthOfLongestSubString.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  LeetCode-Swift
+//
+//  Created by Santosh Ganti on 12/4/19.
+//  Copyright © 2019 SantoshGanti. All rights reserved.
+//
+
+// import Foundation
+
+public class LengthOfLongestSubString {
+    public init() {}
+
+    public func lengthOfLongestSubString(_ s: String) -> Int {
+        if s.count == 0 {
+            return 0
+        } else if s.count == 1 {
+            return 1
+        }
+
+        var maxLength = 0
+        var temp = [Character]()
+        let charArray = Array(s)
+        temp.append(charArray[0])
+
+        for i in 1...charArray.count - 1 {
+            if let index = temp.firstIndex(of: charArray[i]) {
+                temp.removeFirst(index + 1)
+            }
+            temp.append(charArray[i])
+            maxLength = max(maxLength, temp.count)
+        }
+        return maxLength
+    }
+}
